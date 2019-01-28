77c1545f46f5a79661ff5e08213ba7622032ec4b
Abduul6 <43846068+Abduul6@users.noreply.github.com>
2019-01-28 23:54:40 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,75 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+/*1   /*int a=890;
+	    int b=6;
+		System.out.println("a ="+a+" b ="+b);
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		System.out.println("a*b="+(a*b));
+		System.out.println("a/b="+(a/b));
+		System.out.println("a%b="+(a%b));*/
+/*2		/*double metres=50;
+		double feet;
+		System.out.println("enter the metres  "+metres);
+		feet=metres*3.28084;
+		System.out.println(metres+" metres is "+feet+" feet" );*/
+/*3&4		/*int a;
+		int b;
+		int c;
+	     a=(int)(Math.random()*6) +1;
+		b=(int)(Math.random()*6) +1;
+		
+		System.out.println("first die is "+ a);
+		System.out.println("second die is "+b);
+		for(int i=0;i>10;i++)*/
+		
+/*5		/*int y;
+		
+		int j;
+for (int i=0;i<5;i++)
+{
+    for (j=0;j<i;j++)
+    {
+        System.out.print("*");
+    }
+        System.out.println("");
+}
+        for (int x=7;x>0;x--)
+{
+    for (y=0;y<x;y++)
+    {
+        System.out.print("*");
+    }
+        System.out.println("");
+}*/
+ /*7   /*int x= 40;
+double j= 28.9;
+float k=8.7;
+double g;
+System.out.println("my sequence is ");
+for (int i =0;i<x;i++){
+g=j+i*k;
+System.out.println("number"+i+"is"+k);
+}*/
+/*8  for (int i=1;i<=7;i++)
+{
+    for (int j=1;j<=7;j++)
+    {
+        System.out.println("%4d"+i+"is"+k);
+    }
+}
+    }
+
+}	
+	
+	
+
