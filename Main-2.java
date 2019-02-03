517508445dd909b8c1e9ea29ae21d46d3138951d
Abduul6 <43846068+Abduul6@users.noreply.github.com>
2019-02-03 22:52:32 +0000

    Add files via upload

---
+++ Main-2.java
@@ -0,0 +1,82 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/ 
+import java.util.Scanner;
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+	 /*1  /* int a,b;
+	    double x,area ,vol,pi=3.14;
+	    Scanner s= new Scanner(System.in);
+	    
+		System.out.println("enter height of cone");
+		b=s.nextInt();
+		System.out.println("enter radius of cone");
+		a=s.nextInt();
+		x=Math.sqrt((a*a)+(b*b));
+		area = (pi*a*x)+(pi*a*a);
+		System.out.println("surface area of cone:"+area);
+		vol= 0.333*pi*a*a*b;
+			System.out.println("volume of cone:"+vol);
+			*/
+			/* 2    double f;
+			 Scanner s= new Scanner(System.in);
+			 System.out.print("enter amount");
+			 double a=s.nextDouble();
+			 	 System.out.print("enter yearly Interest rate");
+			 double r =s.nextDouble();
+			 r= r/100/12;
+			 	 System.out.print("enter years");
+			int n= s.nextInt();
+			n=n*12;
+			System.out.println("the monthly repayment is : "+(f=a*(1+r)^n*r/((1+r)^n)-1));
+			*/
+			
+		/*3	    Scanner s= new Scanner(System.in);
+			int y,z;
+			System.out.print("enter a three digit number:");
+			 int x=s.nextInt();
+			 z=x% 1000;
+			 y=z/100;
+			 System.out.println("\n the digit in the hundred's place is"+y);
+			  z=z% 100;
+			 y=z/10;
+			 System.out.println("\n the digit in the tenth place is"+y);
+			  y=z%10;
+			 System.out.println("\n the digit in the unit place is"+y);*/
+		/*4   	Scanner myScanner=new Scanner(System.in);
+			System.out.print("enter the amount  of the items");
+			double totalAmt= myScanner.nextDouble();
+			System.out.print("enter the amount  given to the cashier");
+			double amtGiven= myScanner.nextDouble();
+			double penny = 0.01;
+			double changeDue=((double)((int) Math.round((amtGiven-totalAmt)*100))/100.0);
+				double modPence =((double)((int) Math.round((changeDue % penny)*100))/100.0);
+				double numPence=(int)((changeDue-modPence))/(penny);
+				System.out.println(" the  total amount  of change to give is "+changeDue);
+				System.out.println("the number of pence to give is"+numPence);
+				*/
+			/*5	  Scanner s= new Scanner(System.in);
+				  int num1,num2,q,p,num3;
+				  int num4;
+				  q=s.nextInt();
+				  p=s.nextInt();
+				  	System.out.println("enter number for q"+q);
+				  		System.out.println("enter number for p"+p);
+				  		num1=(int)(Math.random()*2)+0;
+				  			num2=(int)(Math.random()*q)+52;
+				  				num3=(int)(Math.random()*q)+1;
+				  						num3=(int)(Math.random()*q)+p;
+				  			System.out.println("the random num between 1 & 0 is "+num1);
+				  				System.out.println("the random num between 0 & q is "+num2);
+				  					System.out.println("the random whole num between 0 & q is "+num3);
+				  						System.out.println("the random num between q & p is "+num4);
+	*/
+	}
+}
