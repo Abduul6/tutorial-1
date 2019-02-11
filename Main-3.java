fd0a51ecc1dc80d890603e31a9b2500eb9bfe1a3
Abduul6 <43846068+Abduul6@users.noreply.github.com>
2019-02-11 22:44:38 +0000

    Add files via upload

---
+++ Main-3.java
@@ -0,0 +1,141 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+/*	1    float x;
+	    Scanner in= new Scanner (System.in);
+		System.out.println("enter a float number");
+		x=in.nextFloat();
+		if(x=0){
+		System.out.println("zero");
+		}else if(x>0){
+		System.out.println("positive");
+		}else{
+		    System.out.println("negative");
+		    
+		}*/
+	/*1b	int x,y,z;
+		Scanner in= new Scanner (System.in);
+		System.out.println("enter a number");
+		x=in.nextInt();
+		
+		System.out.println("enter a number");
+		y=in.nextInt();
+	
+		System.out.println("enter a number");
+		z=in.nextInt();
+		}
+		if(x ==y & y ==z & x==z){
+		System.out.println("all the same");
+		}else if (x !==y & y !==z & x!==z){
+		System.out.println("all different");
+		    {else System.out.println("neither")
+		    */
+ /*2     int die1;  
+        int die2;   
+        int roll;  
+        
+        die1 = (int)(Math.random()*6) + 1;
+        die2 = (int)(Math.random()*6) + 1;
+        roll = die1 + die2;
+        
+        System.out.println("The first die is" + die1);
+        System.out.println("The second die is " + die2);
+        System.out.println("Your total roll is " + roll);
+        if (die1>die2){
+            System.out.println("die 1 is greater than die 2");
+        }
+        else if (die1<die2){
+            System.out.println("die 1  is less than  die 2");
+        }*/
+    /*3    Scanner s = new Scanner(System.in);
+ 
+     int a; 
+    
+        a = s.nextInt();
+       System.out.println("enter your score" +a);     
+     
+       
+        
+if (a<40){
+    System.out.println("you have failed ");}
+
+ if ((a>40) && (a<75)){
+    System.out.println("this is a pass");
+    }
+      else if ((a>75) || (a==100)){
+          System.out.println("distinction");
+
+      }*/
+/*4    Scanner s = new Scanner(System.in);
+ 
+     int num; 
+    
+     
+        
+       
+        num = s.nextInt();
+            System.out.println("enter your score" +num);
+     
+       
+        
+    if (num<40){
+    System.out.println("you have failed ");
+      }
+      else if(num>=40){
+    System.out.println("this is a pass");
+    }
+      else if (num>=75){
+          System.out.println("distinction");
+
+      }*/
+      
+ 
+     
+    /*5   Scanner s = new Scanner(System.in);
+       float x,y;
+            System.out.println("pick a number for x: " +x);
+            x= s.nextFloat();
+         System.out.println("pick a number for y: " +y);
+          
+         y= s.nextFloat();
+        
+if ('x'<=0){
+    System.out.println("this is frozen");
+}
+else if('x'<=99){
+    System.out.println("this is liquid");
+    }
+      else if ('x'==100){
+          System.out.println("this is gas");
+          
+}
+else {
+System.out.println("this is an error");
+}
+if ('y'<=32){
+    System.out.println("this is frozen");
+}
+else if('y'<=125){
+    System.out.println("this is liquid");
+    }
+      else if ('y'==200){
+          System.out.println("this is gas");
+          
+}
+else {
+System.out.println("this is an error");
+}
+       */
+		}
+	}
+
